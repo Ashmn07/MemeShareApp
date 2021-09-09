08b03be3a5be1060046a9472a233baa806306794
Ashmn07 <ashwin.menon202@gmail.com>
2021-09-09 22:58:06 +0530

    Added Glide and Volley libraries

--- app/src/main/java/com/example/memeshareapp/MainActivity.kt
+++ app/src/main/java/com/example/memeshareapp/MainActivity.kt
@@ -2,10 +2,46 @@ package com.example.memeshareapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getMeme()
     }
+
+    fun getMeme(){
+
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://meme-api.herokuapp.com/gimme"
+
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET, url, null,
+            { response ->
+                val url = response.getString("url")
+                Glide.with(this).load(url).into(memeImageView)
+            },
+            { error ->
+                Toast.makeText(this,"Something has gone wrong",Toast.LENGTH_LONG).show()
+            }
+        )
+        queue.add(jsonObjectRequest)
+    }
+    fun nextMeme(view: View) {
+
+    }
+
+    fun shareMeme(view: View) {
+
+    }
+
 }
